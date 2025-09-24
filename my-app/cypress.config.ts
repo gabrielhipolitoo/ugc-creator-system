d3c7b1d501f1aa085eb7946ae3d61708936c577b
Gabriel <dev.gabrielhipolito@gmail.com>
2025-09-24 18:53:47 -0300

    alterando arquifo de configuração do cypress

--- my-app/cypress.config.ts
+++ my-app/cypress.config.ts
@@ -4,7 +4,10 @@ export default defineConfig({
   chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: "http://localhost:3000/",
+    baseUrl: "http://localhost:3000",
     testIsolation: false,
+  },
+  hosts: {
+    localhost: "localhost:3000",
   },
 });
